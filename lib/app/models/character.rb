81ca6139dc1fda79dcbe6e47503e214417f19f3f
Caryn McCarthy <cmccarthy15@gmail.com>
2020-09-28 13:05:36 -0400

    removed random katara stuff

--- lib/app/models/character.rb
+++ lib/app/models/character.rb
@@ -3,8 +3,5 @@ class Character < ActiveRecord::Base
     has_many :nations, through: :affiliations
     has_many :relationships
 
-    def is_katara
-        return self.name.include?("Katara")
-    end
 end
 
