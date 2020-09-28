d7ceecdd03e0f451397ae879d6ca3704e61176cf
Caryn McCarthy <cmccarthy15@gmail.com>
2020-09-28 13:03:29 -0400

    add katara method in Character

--- lib/app/models/character.rb
+++ lib/app/models/character.rb
@@ -2,4 +2,9 @@ class Character < ActiveRecord::Base
     has_many :affiliations
     has_many :nations, through: :affiliations
     has_many :relationships
+
+    def is_katara
+        return self.name.include?("Katara")
+    end
 end
+
