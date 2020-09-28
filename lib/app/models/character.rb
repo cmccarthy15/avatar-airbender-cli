5a13b5eb73c7dbd20f7f667a2bc37b9528f2cdf3
Caryn McCarthy <cmccarthy15@gmail.com>
2020-09-28 14:41:31 -0400

    added those character class methods

--- lib/app/models/character.rb
+++ lib/app/models/character.rb
@@ -3,5 +3,14 @@ class Character < ActiveRecord::Base
     has_many :nations, through: :affiliations
     has_many :relationships
 
+    def self.water_tribe_members
+        self.all.filter { |char| character.nation == "Water Tribe" }
+    end
+
+    def self.fire_nation_members
+        self.all.filter { |char| character.nation == "Fire Nation" }
+    end
+
+    ## Pretend we have air nomads and earth kingdom 
 end
 
